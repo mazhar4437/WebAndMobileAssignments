def8620d563ccd5cf51db68354e73ee59ee3e0ec
Mazhar4437 <k132193@nu.edu.pk>
2020-09-27 16:27:34 +0500

    Firebase ChatApp with facebook authentication

--- Javascript/Firebase Database/Facebook Authentication/app.js
+++ Javascript/Firebase Database/Facebook Authentication/app.js
@@ -1,22 +1,98 @@
+
+
 let login = () =>
 {
+  
 var provider = new firebase.auth.FacebookAuthProvider();
+
+//let promise = new Promise((resolve,reject)=>
+//{
+  console.log("Hi No Promise");
 firebase.auth().signInWithPopup(provider).then(function(result) {
     // This gives you a Facebook Access Token. You can use it to access the Facebook API.
     var token = result.credential.accessToken;
     // The signed-in user info.
     var user = result.user;
+    
     console.log(user);
+    document.getElementById("login").style.display = "none";
+    document.getElementById("maindiv").style.visibility = "visible";
+    debugger;
+    let myName = user.displayName;
+    let myemail = user.email;
+    let myuserID = user.uid;
+    let myphoto = user.photourl;
+   // resolve(result);
+    document.getElementById("displayname").innerHTML = user.displayName;
+    
    
-  }).catch(function(error) {
+    
+        
+
    
+  })
+  
+  .catch(function(error) {
+   
+  
     var errorCode = error.code;
     var errorMessage = error.message;
     // The email of the user's account used.
     var email = error.email;
     // The firebase.auth.AuthCredential type that was used.
     var credential = error.credential;
-   console.log(errorMessage)
-    // ...
-  });
+   console.log(errorMessage);
+   //reject(error);
+  
+ // }
+ // );
+ 
+});
+//promise
+//.then((data)=>
+//{
+  //console.log("Resolve = >",data)
+firebase.database().ref('messages').on('child_added',function(data)
+{
+  debugger;
+  console.log(JSON.stringify(data.val()))
+  let carddiv = document.getElementById("carddiv");
+ 
+  let cardtextbody = document.createElement("p");
+  cardtextbody.className = "card-text";
+  let textnode =  document.createTextNode((data.val().sender) + " : " + (data.val().message));
+  cardtextbody.append(textnode);
+  carddiv.append(cardtextbody);
+ 
+}
+)
+
+//.catch((error)=>console.log("Error =>",error))
+}
+
+
+
+let btnsend  = () => 
+{
+
+ //let carddiv = document.getElementById("carddiv");
+  let message = document.getElementById("message");
+//  let cardtextbody = document.createElement("p");
+  //cardtextbody.className = "card-text";
+ // let textnode =  document.createTextNode( document.getElementById("displayname").innerHTML + " : " + message.value);
+ //// cardtextbody.append(textnode);
+ 
+ 
+
+let key = firebase.database().ref("messages").push().key;
+
+let messages = 
+{
+    key: key,
+
+    sender : document.getElementById("displayname").innerHTML,
+    message :message.value
+}
+firebase.database().ref("messages/"+key).set(messages);
+message.value = "";
 }
---
+++ Javascript/Firebase Database/Facebook Authentication/chatApp.js
@@ -0,0 +1,32 @@
+// let messages = [
+//     {
+//       messageID : 1,
+//       userID : 1,
+//       messages : "hi"
+
+// },
+// {
+//     messageID : 2,
+//       userID : 1,
+//       messages : "hello"
+// },
+// {
+//     messageID : 3,
+//       userID : 2,
+//       messages : "hey"
+// }
+// ]
+// let myName = document.getElementById("myName");
+// let message = document.getElementById("message");
+// let key = firebase.database().ref("messages").push().key;
+
+// let messages = 
+// {
+//     key: key,
+//     userID : "l2QffqEDmnchMfywk7Wih0S9M7G3",
+//     sender : "Mazhar",
+//     message : "Hi"
+// }
+// firebase.database().ref("messages/"+key).set(messages);
+
+
